2935ab602e2340d00cd4dadee326b1a34c4728b2
brendon <108491991+BrendonTheDev@users.noreply.github.com>
2024-02-09 09:37:11 -0500

    Succesfully populated db with code from atlas. Kept Mongo code from node tutorial and mdn docs as backup in comments.

--- myapp/app.js
+++ myapp/app.js
@@ -5,17 +5,51 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+//MONGOOSE FROM BLOG
+// const dbURI =
+//   "mongodb+srv://brendon:Wetdog22@nodelearn.themebq.mongodb.net/node-learn?retryWrites=true&w=majority";
+// mongoose
+//   .connect(dbURI)
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log(err));
+
 // mongoose setup
 mongoose.set("strictQuery", false);
 
-// define database url to connect to
-const mongoDB =
-  "mongodb+srv://brendon:Wetdog22@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
-// wait for database URL to connect to.
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
+// const mongoDB = process.env.MONGODB_URI || db_url;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://brendon:Wetdog22@locallibrarycluster.kgsgd20.mongodb.net/Local_Library?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
+
+// MONGOOSE FROM MDN DOCS
+// wait for database URL to connect to.
+// main().catch((err) => console.log(err));
+// async function main() {
+//   await mongoose.connect(db_url);
+// }
 
 // access routers
 const indexRouter = require("./routes/index");
