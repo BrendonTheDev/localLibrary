f99c20bdb00dea1624a92ca8eff9cdfa2e59b4c0
brendon <108491991+BrendonTheDev@users.noreply.github.com>
2024-02-12 11:53:14 -0500

    Reconfigure mongoose/mongo connection in app.js.

--- myapp/app.js
+++ myapp/app.js
@@ -3,61 +3,37 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 
-//MONGOOSE FROM BLOG
-// const dbURI =
-//   "mongodb+srv://brendon:Wetdog22@nodelearn.themebq.mongodb.net/node-learn?retryWrites=true&w=majority";
-// mongoose
-//   .connect(dbURI)
-//   .then((result) => app.listen(3000))
-//   .catch((err) => console.log(err));
-
-// mongoose setup
-mongoose.set("strictQuery", false);
-
-// const mongoDB = process.env.MONGODB_URI || db_url;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://brendon:Wetdog22@locallibrarycluster.kgsgd20.mongodb.net/Local_Library?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// MONGOOSE FROM MDN DOCS
-// wait for database URL to connect to.
-// main().catch((err) => console.log(err));
-// async function main() {
-//   await mongoose.connect(db_url);
-// }
-
-// access routers
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog");
+const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
 
-// instantiate express app
+const compression = require("compression");
+const helmet = require("helmet");
+
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 10 * 1000, // 10 seconds
+  max: 10,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const dev_db_url =
+  "mongodb+srv://brendon:Wetdog22@locallibrarycluster.kgsgd20.mongodb.net/Local_Library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -67,11 +43,22 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
