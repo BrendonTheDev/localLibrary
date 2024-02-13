e0a8a5fefb68ab1258caa8ff91453a5446001aab
brendon <108491991+BrendonTheDev@users.noreply.github.com>
2024-02-13 16:49:03 -0500

    Format dates with luxon.

--- myapp/models/author.js
+++ myapp/models/author.js
@@ -26,21 +26,21 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
-// AuthorSchema.virtual("lifespan").get(function () {
-//   let lifetime_string = "";
-//   if (this.date_of_birth) {
-//     lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
-//       DateTime.DATE_MED
-//     );
-//   }
-//   lifetime_string += " - ";
-//   if (this.date_of_death) {
-//     lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
-//       DateTime.DATE_MED
-//     );
-//   }
-//   return lifetime_string;
-// });
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
 
 // AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
 //   return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
