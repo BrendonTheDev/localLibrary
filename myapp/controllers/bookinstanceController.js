a46c795ed09fd0f702e52e653d44cce859205b6e
brendon <108491991+BrendonTheDev@users.noreply.github.com>
2024-02-19 13:48:25 -0500

    Create bookInstance delete controllers. Add delete and update btns to bookinstance_detail. Create bookinstance delete view.

--- myapp/controllers/bookinstanceController.js
+++ myapp/controllers/bookinstanceController.js
@@ -95,12 +95,25 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const bookinstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("catalog/bookinstances");
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete BookInstance",
+    bookinstance: bookInstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  await BookInstance.findByIdAndDelete(req.body.id);
+  res.redirect("catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
